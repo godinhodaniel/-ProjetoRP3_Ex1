712bad2a198179e856205fbdf0e1f6bb7db03091
Daniel Godinho <144964197+godinhodaniel@users.noreply.github.com>
2024-08-25 23:51:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace readwriteapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter wr = new StreamWriter(@"D:\doc.txt", true);
+            wr.WriteLine("Primeira linha");
+            wr.WriteLine("Segunda linha");
+            wr.Close();
+            StreamReader rd = new StreamReader(@"D:\doc.txt");
+            while (!rd.EndOfStream)
+            {
+                string linha = rd.ReadLine();
+                Console.WriteLine(linha);
+            }
+            rd.Close();
+            Console.Write("\n\n\nPressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
